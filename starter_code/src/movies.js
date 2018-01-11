348d9901e09c2f616903658b2e11fe94ca5abe4f
Alvaro <echanovevaro@gmail.com>
2018-01-12 00:26:07 +0100

    segunda y tercera iteración

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
-
 // Get the average of all rates with 2 decimals 
+
+function ratesAverage(arrayMovies){
+    var average = arrayMovies.map(function(movie){
+      return movie.rate;
+    }).reduce(function(total, rate){
+       return rate/arrayMovies.length + total;
+    },0);
+
+    var averageRounded = Math.round(average * 100) / 100;
+
+    console.log(averageRounded);
+
+    return averageRounded;
+} 
 
 
 // Get the average of Drama Movies
+
+function dramaMoviesRate(arrayMovies){
+    var dramaArray = arrayMovies.filter(function(movie){
+            return movie.genre.indexOf("Drama") !== -1;
+        });
+    var dramaAverage = dramaArray.map(function(movie){
+            return movie.rate;
+        }).reduce(function(total, rate){
+            return rate/dramaArray.length + total;
+        },0);
+
+        var averageRounded = Math.round(dramaAverage * 100) / 100;
+
+        console.log(averageRounded);
+    
+        return averageRounded;
+}
 
 
 // Order by time duration, in growing order
